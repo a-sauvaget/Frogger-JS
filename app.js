abdc9f156a896e9d3446d8dd6bd58a8576da6f70
a-sauvaget <alexandre.sauvaget44@gmail.com>
2022-02-26 20:06:55 +0100

    [FEAT] Frog movement done.

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const GRID_WIDTH = 9;
+const GRID_HEIGHT = 9;
 
 const timeLeftDisplay = document.getElementById('time-left');
 const scoreDisplay = document.getElementById('score');
@@ -7,27 +8,31 @@ const startPauseButton = document.getElementById('start-pause-button');
 const squares = document.querySelectorAll('.grid div');
 
 // Position de départ de la grenouille
-let currentIndex = 76;
+let currentIndex = Math.ceil(GRID_HEIGHT * GRID_WIDTH - (GRID_WIDTH / 2 + 1));
 
 function moveFrog(e) {
   squares[currentIndex].classList.remove('frog');
 
   switch (e.key) {
     case 'ArrowLeft':
-      console.log('Moved left !');
-      currentIndex -= 1;
+      if (currentIndex % GRID_WIDTH !== 0) {
+        currentIndex -= 1;
+      }
       break;
     case 'ArrowRight':
-      console.log('Moved right !');
-      currentIndex += 1;
+      if (currentIndex % GRID_WIDTH < GRID_WIDTH - 1) {
+        currentIndex += 1;
+      }
       break;
     case 'ArrowUp':
-      console.log('Moved up !');
-      currentIndex -= GRID_WIDTH;
+      if (currentIndex - GRID_WIDTH >= 0) {
+        currentIndex -= GRID_WIDTH;
+      }
       break;
     case 'ArrowDown':
-      console.log('Moved down !');
-      currentIndex += GRID_WIDTH;
+      if (currentIndex + GRID_WIDTH < GRID_WIDTH * GRID_HEIGHT) {
+        currentIndex += GRID_WIDTH;
+      }
       break;
     default:
       console.log('Use the arrows to move the frog !');
